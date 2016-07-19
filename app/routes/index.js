3ab3f7ab3a9682bdb385f01ce5a63e6947c0a97d
Ryan Tablada <ryan.tablada@gmail.com>
2016-07-19 11:55:20 -0500

    Create handlebars helper function

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,23 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return [
-      {
-        img: 'http://placecera.com/gif/200/200',
-        title: 'I am an object'
-      },
-      {
-        img: 'http://www.stevensegallery.com/200/200',
-        title: 'Segal!'
-      },
-      {
-        img: 'http://www.fillmurray.com/200/200',
-        title: 'Bill!'
-      },
-      {
-        img: 'http://www.placekitten.com/200/200',
-        title: 'AWW!'
-      },
-    ];
+    return fetch('https://api.imgur.com/3/gallery/hot/viral/0.json')
+      .then((res) => res.json());
   }
 });
---
+++ tests/unit/helpers/not-test.js
@@ -0,0 +1,10 @@
+import { not } from 'lesson-notes-ember/helpers/not';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | not');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = not([42]);
+  assert.ok(result);
+});
